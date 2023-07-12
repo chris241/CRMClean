38ee74edf1c75617268361e49124b8aed6e722b6
chris241 <chrisleon325@gmail.com>
2023-07-12 15:26:15 +0400

    Add project files.

---
+++ CRM.Presistence/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CRM.Presistence;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection services)
+    {
+        return services;
+    }
+}
