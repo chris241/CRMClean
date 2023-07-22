5b6bcc3eb910c7cae12ac8c7e2c06e8d2a376c9f
chris241 <chrisleon325@gmail.com>
2023-07-22 10:11:42 +0400

    Add Departement

--- CRM.Domain/Employee/Employee.cs
+++ CRM.Domain/Employee/Employee.cs
@@ -3,11 +3,11 @@
     public class Employee : Entity, IAggregateRoot
     {
         public EmployeeId Id { get; set; }
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
-        public string Email { get; set; }
-        public string Address { get; set; }
-        public string Fonction { get; set; }
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
+        public string? Fonction { get; set; }
 
     }
 }
